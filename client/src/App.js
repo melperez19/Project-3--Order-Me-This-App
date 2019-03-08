d1b98efca28a75e6775c4d3019472237e8c72141
danrsolomon <dansolomon76@yahoo.com>
2019-03-07 20:28:28 -0700

    changes to app.js to handle login

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+export default class App extends Component {
+  state = {
+    errors: null,
+    isMounted: false,
+    user: {
+      anonymous: true
+    },
+  }
 
-export default App;
+  componentDidMount() {
+    const user = getCurrentUser()
+    this.setState({ isMounted: true })
+  }
+
+  getCurrentUser = async () => {
+    const response = await fetch('/api/user')
+    if (response.status === 200) {
+      const user = await response.json()
+      this.setState({ user: { ...user, anonymous: false } })
+    } else {
+      const errors = await response.json()
+      this.setState({ errors })
+      // handle other response
+    }
+  }
+
+  render = () => {
+    if (this.state.isMounted) {
+      return <div>Loading...</div>
+    }
+
+    if (this.state.user.anonymous) {
+      return <LoginPage errors={this.state.errors} />
+    }
+
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            {/* <Route exact path="/" component={Books} />
+            <Route exact path="/books" component={Books} />
+            <Route exact path="/books/:id" component={Detail} /> */}
+            <Route exact path="/" component={Email} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
