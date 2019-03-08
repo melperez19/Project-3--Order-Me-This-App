49c187aa65ce83d760fea33a6a98f6b33adbd3c6
danrsolomon <dansolomon76@yahoo.com>
2019-03-07 20:07:42 -0700

    Add files from MERN setup

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const signinRoutes = require('./signin');
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use('/signin', signinRoutes);
 
 module.exports = router;
