86694add7a2505718e6c79bb4122e0aba2694ad5
Melperez19 <melperez19@gmailcom>
2019-03-14 23:18:58 -0400

    fix

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  console.log("test");
 }
 // Add routes, both API and view
 app.use(routes);
