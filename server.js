3f3b7ce1c70be0cacfa6d61d2620a3f94fc3fa52
Melperez19 <melperez19@gmailcom>
2019-03-14 22:00:53 -0400

    updated db name

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/order_me_this_db");
 
 // Start the API server
 app.listen(PORT, function() {
