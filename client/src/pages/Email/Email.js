f93b6fd5bc257e0e440789a40ed9bbe27847938d
Dan R Solomon <dansolomon76@yahoo.com>
2019-03-26 19:14:29 -0700

    changed email link to work in heroku

--- client/src/pages/Email/Email.js
+++ client/src/pages/Email/Email.js
@@ -78,7 +78,7 @@ class Email extends Component {
                 restaurant_menu_URL: restaurantMenuURL,
                 to_email: sendToEmail,
                 from_name: fromName,
-                order_me_this_event_link: "https://powerful-journey-65247.herokuapp.com/",
+                order_me_this_event_link: "https://powerful-journey-65247.herokuapp.com/event",
                 // order_me_this_event_link: "http://localhost:3000/event",
                 message_html: message
             }
